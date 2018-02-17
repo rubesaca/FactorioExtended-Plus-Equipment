a9036c96b33d6e9780a57b14e15b6442ea3192e1
jimmyjon711 <jcmadill1@gmail.com>
2018-02-16 21:08:23 -0700

    Resolved issue #1
    Bumped power armor grid to 14 so that there is no longer an empty space on top and bottom

--- prototypes/entity/entity-equipment-grid.lua
+++ prototypes/entity/entity-equipment-grid.lua
@@ -3,8 +3,8 @@ data:extend(
   {
     type = "equipment-grid",
     name = "xlarge-equipment-grid",
-    width = 13,
-    height = 13,
+    width = 14,
+    height = 14,
     equipment_categories = {"armor"}
   }
 }
